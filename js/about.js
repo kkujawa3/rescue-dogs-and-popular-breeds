cfe0f94bfccbfe6ad5ff5b7ebedaab566c8ac3fa
unknown <kimberllee.al@gmail.com>
2025-03-13 12:08:29 -0400

    Added about and dogfun js files and started their layouts.

---
+++ js/about.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const stars = document.querySelectorAll('.star');
+const ratingValue = document.getElementById('rating-value');
+const resetButton = document.getElementById('reset-button');
+
+let currentRating = 0;
+
+
+// Click event to set the rating
+
+// Function to update the stars based on rating from user
+
+// Clear rating from reset button for next user
+
+
+
---
+++ js/dogfun.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const question1Element = document.getElementById('question1');
+const question2Element = document.getElementById('question2');
+const question3Element = document.getElementById('question3');
+
+// Submit button
+
+// Retrieve answers to questions in an array
+
+// Display answer from question 1
+
+// Doughnut Chart answer from question 2
+
+// Bar Chart answer from question 3
+
+// Reset form for next user
