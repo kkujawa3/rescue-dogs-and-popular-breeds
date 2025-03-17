71f2b3b73cdbf51dbfd0873659aece4e40fba77c
unknown <kimberllee.al@gmail.com>
2025-03-17 18:30:46 -0400

    Continue with updates for home, about and dog fun pages for functionality.

--- js/dogfun.js
+++ js/dogfun.js
@@ -1,14 +1,26 @@
 "use strict";
 
-const question1Element = document.getElementById('question1');
+// Displays answer from Form Question 1
+document.getElementById('questionForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let name = document.getElementById('question1').value;
+    document.getElementById('displayQuestion1').innerText = 'Your Rescue Dog Name is: ' +name + ' !';
+    document.getElementById('questionForm').reset();
+});
+
+function clearDisplay() {
+  document.getElementById('displayQuestion1').innerText = '';
+}
+
+document.getElementById('reset-button').addEventListener('click', clearDisplay);
+
+//const question1Element = document.getElementById('question1');
 const question2Element = document.getElementById('question2');
 const question3Element = document.getElementById('question3');
 
 // Submit button
 
 // Retrieve answers to questions in an array
-
-// Display answer from question 1
 
 // Doughnut Chart answer from question 2
 
