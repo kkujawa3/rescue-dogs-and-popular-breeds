0a4eba5b9915dda8a516b1df0107ef7dfbc0c4ce
unknown <kimberllee.al@gmail.com>
2025-03-19 16:53:46 -0400

    Working on updates to the about and dogfun pages for their functionality.

--- js/dogfun.js
+++ js/dogfun.js
@@ -1,22 +1,10 @@
 "use strict";
 
-
-
-// Displays answer from Form Question 1
-
-  //const question2Element = document.getElementById('question2');
-  //const question3Element = document.getElementById('question3');
-
-  //let name = document.getElementById('question1').value;
-  //document.getElementById('displayQuestion1').innerText = 'Your Rescue Dog Name is: ' +name + ' !';
-  //document.getElementById('questionForm').reset();
-
   // Start here
   // Doughnut Chart answer from Form Question 2
-const doughnutCtx = document.getElementById('myDoughnutChart').getContext('2d');
+/*const doughnutCtx = document.getElementById('myDoughnutChart').getContext('2d');
 const barCtx = document.getElementById('myBarChart').getContent('2d');
 
-//let two = document.getElementById('question2').value;
 let myDoughnutChart = new Chart(doughnutCtx, {
     type: 'doughnut',
     data: {
@@ -29,7 +17,7 @@ let myDoughnutChart = new Chart(doughnutCtx, {
 });
 
 // Bar Chart answer from Form Question 3
-//const barCtx = document.getElementById('myBarChart').getContent('2d');
+
 let myBarChart = new Chart(barCtx, {
     type: 'bar',
     data: {
@@ -39,31 +27,37 @@ let myBarChart = new Chart(barCtx, {
         backgroundColor: ['#ff6385'],
       }]
     }
-});
+});*/
 
+//get form answers
+
+// display answers to Question 1
+//document.getElementById('answer1Display').textContent = `Your Rescue Dog Name is: ${answer1}`;
+
+// Testing the three questions
 document.getElementById('questionForm').addEventListener('submit', function(event) {
   event.preventDefault();
+  //Retrieve and display answer to Question 1
+  let name = document.getElementById('question1').value;
+  document.getElementById('displayQuestion1').innerText = "Your Rescue Dog Name is: " + name + "!";
 
-  //get form answers
+  // Retrieve and display answer to Question 2
+  let breed = document.getElementById('question2').value;
+  document.getElementById('displayQuestion2').innerText = "Your favorite breed is: " + breed + "!";
 
-  // display answers to Question 1
-  document.getElementById('answer1Display').textContent = `Your Rescue Dog Name is: ${answer1}`;
-
-  // Answer for Question 2
-
-  // Answer for Question 3
-
-
+  // Retrieve and display answer to Question 3
+  let trick = document.getElementById('question3').value;
+  document.getElementById('displayQuestion3').innerText = "Dog trick you would teach your rescue is: " + trick + "!";
 })
 
-//function clearDisplay() {
-//  document.getElementById('displayQuestion1').innerText = '';
-//}
-
-//document.getElementById('reset-button').addEventListener('click', clearDisplay);
-
+// clear display
+function clearDisplay () {
+  document.getElementById('displayQuestion1').innerText = '';
+  document.getElementById('displayQuestion2').innerText = '';
+  document.getElementById('displayQuestion3').innerText = '';
+}
+// Reset form next user
+document.getElementById('reset-button').addEventListener('click', clearDisplay);
 
 // Retrieve answers to questions in an array
 
-
-// Reset form for next user
