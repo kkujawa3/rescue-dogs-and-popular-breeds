48878a1489505ae41ed3e801c45186b56ffa2f3c
unknown <kimberllee.al@gmail.com>
2025-03-26 19:44:15 -0400

    Working through api file for functionality.

--- public/js/dogrescue.js
+++ public/js/dogrescue.js
@@ -8,18 +8,24 @@ async function getRandomPhoto() {
     const url = "http://localhost:1776/api/getRandomImage";     
     try {
         const response = await fetch(url);
-        const data: await response.json();
-        const receivedPhoto = data[0].urls;
-        const breed = data[0].breeds[0]?.name || "breed unknown";
-        const temperament = data[0].breeds[0]?.temperament || "Temperament unknown";
-        return {
-            photo: receivedPhoto,
-            breed: breed,
-            temperament: temperament
-        };
+        const data = await response.json();
+        console.log("Response Status:", response.status);
+        console.log("Response JSON Data:", data);
+
+        const receivedPhoto = data.data.photo;
+        const dogBreed = data.data.breed;
+        const dogTemperament = data.data.temperament;
+
+        const dogPic = document.getElementById("dog-photo");
+        const dogBreed = document.getElementById("dog-breed");
+        const dogTemperament = document.getElementById("dog-temperament");
+        
     } catch (error) {
         console.log(error);  
         throw new error("Error getting the dog photo");  
     }
 }
-getRandomPhoto();
+
+document.addEventListener("DOMContentLoaded", function() {
+    getRandomPhoto();
+});
